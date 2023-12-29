58b8b345d9e62371388cba5b509a24473cc4a4cd
Kagan Batuker <kagan@holoride.com>
2023-12-29 19:43:38 +0100

    improved matching algorithm logic and bugfixes
    - added validation for matches
    - cleaned up code
    - added depth threshold
    - if 4 or less matches are missing, add a verified match to the pot and rematch them

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::*;
 use std::collections::VecDeque;
-//use calamine::{Reader, Xlsx, open_workbook};
 use rand::thread_rng;
 use rand::seq::SliceRandom;
-
 
 use spreadsheet_ods::sheet::Range;
 use spreadsheet_ods::{WorkBook, Sheet, Value};
@@ -12,11 +10,12 @@ use spreadsheet_ods::format;
 use spreadsheet_ods::formula;
 use spreadsheet_ods::{cm, mm};
 use spreadsheet_ods::style::{CellStyle};
-use spreadsheet_ods::color::Rgb;
 use icu_locid::locale;
 use spreadsheet_ods::style::units::{TextRelief, Border, Length};
 
-const TABLE_SIZE: u32 = 9;
+const TABLE_SIZE: u32 = 49; //  length of row -1
+const MAX_DEPTH: usize = 8; //    maximum number of traversals for the search
+const DATE: &str = "99.99.24";
 
 fn main()
 {
@@ -27,48 +26,52 @@ fn main()
     	.ok()
         .expect("Failed to read line");
 
-
-    let loc = locale!("en-US"); 
-    let path = std::path::Path::new("odsRustSheet.ods");
+    let path = std::path::Path::new("rust_lottery.ods");
     let mut wb = if path.exists() {
         spreadsheet_ods::read_ods(path).unwrap()
         } 
         else {
-        WorkBook::new(loc)
+        WorkBook::new(locale!("en-US"))
     };
 
-
     // populate participants list.
-    let partip_sheet = wb.sheet(1);
+    let partip_sheet = wb.sheet(6); // hard coded
     let mut participants: Vec<String> = Vec::new();
-    for r in 0..TABLE_SIZE{
-        match partip_sheet.value(r, 0) {
+
+    for r in 0..TABLE_SIZE {
+        let v = get_value(r, 0, partip_sheet).to_string();
+        if !v.eq("")
+        {
+            participants.push(v);
+        }
+    }
+
+    let main_sheet = wb.sheet(4); // hard coded
+    let mut idx=0;
+
+    for r in 0..=TABLE_SIZE{
+        match main_sheet.value(r, 0) {
             spreadsheet_ods::Value::Empty => {}
             spreadsheet_ods::Value::Boolean(v) => {}
             spreadsheet_ods::Value::Number(v) => {}
             spreadsheet_ods::Value::Percentage(v) => {}
             spreadsheet_ods::Value::Currency(v, cur) => {}
             spreadsheet_ods::Value::Text(v) => {
-                println!("({}) => {}", r, v);
-                participants.push(v.to_string());
+                println!("[TEST] person_name:{} found index:{}", v, idx);
             }
             spreadsheet_ods::Value::TextXml(v) => {}
             spreadsheet_ods::Value::DateTime(v) => {}
             spreadsheet_ods::Value::TimeDuration(v) => {}
         }
+
+        idx += 1;
     }
 
-    let main_sheet = wb.sheet(0);
-    //participants.shuffle(&mut thread_rng());
     let matches = matching(&participants, &main_sheet, 0);
+    //validate matches
+    assert!(matches.iter().all(|item| participants.contains(item)));
     //Print out matches.
     print_matches(&matches, true);
-
-    // let test_value = main_sheet.value(0, 2).as_str_or("");
-    // println!("Test value: {}", &test_value);
-
-    // let mut excel_xlsx: Xlsx<_> = open_workbook("rust_lottery.xlsx").unwrap();
-    //let mut excel_table = vec![vec![String::from(""); ROW_SIZE]; ROW_SIZE];
 
     for r in 0..TABLE_SIZE{
         for c in 0..TABLE_SIZE {
@@ -91,102 +94,169 @@ fn main()
         }
     }
 
-    // get converted value
-    let a1 = main_sheet.value(0, 0).as_str_or("");
-    //println!("A1 {}", a1);
+    //TODO update excel table.
 
     // let mut sheet = Sheet::new("sample");
     // sheet.set_value(2, 2, "sample");
     // wb.push_sheet(sheet);
     // spreadsheet_ods::write_ods( & mut wb, "tryout.ods");
 
-    // // Matching is done here.
-    // let matches = validate_matches(&participants, &excel_table, 0);
-    // //Print out matches.
-    // print_matches(&matches, true);
-
-    //TODO fill excel table back up.
-
 } 
+
+fn get_value(row : u32, col : u32, sheet : &Sheet ) -> &str {
+        match sheet.value(row, col) {
+            spreadsheet_ods::Value::Empty => {}
+            spreadsheet_ods::Value::Boolean(v) => {}
+            spreadsheet_ods::Value::Number(v) => {}
+            spreadsheet_ods::Value::Percentage(v) => {}
+            spreadsheet_ods::Value::Currency(v, cur) => {}
+            spreadsheet_ods::Value::Text(v) => {
+                return sheet.value(row, col).as_str_or("");
+            }
+            spreadsheet_ods::Value::TextXml(v) => {}
+            spreadsheet_ods::Value::DateTime(v) => {}
+            spreadsheet_ods::Value::TimeDuration(v) => {}
+        }
+        return "";
+}
+
+fn simple_match(participants : &Vec<String>, sheet : &Sheet) -> Vec<String> {
+
+    let mut return_vec: Vec<String> = Vec::new();
+    let mut incorrect_matches: Vec<String>;
+    let mut partip_copy = participants.clone();
+    let mut count : u32 = 0;
+
+    // loop until a combination of matches is reached without any incorrect matches.
+    loop
+    {
+        count +=1;
+        let mut index = 0;
+        incorrect_matches = Vec::new();
+        return_vec = Vec::new();
+        let length: usize = participants.len();
+        println!("matching: |\tlength {} ", length);
+
+        partip_copy.shuffle(&mut thread_rng());
+
+        while index < length {
+            if length > index+1 {
+    
+                let first = get_person_index(&partip_copy[index], sheet);
+                let second = get_person_index(&partip_copy[index+1], sheet);
+    
+                let already_matched = get_value(first, second, sheet).ne("");
+    
+                //println!("{} and {} MATCH? {}.", &partip_copy[index], &partip_copy[index+1], get_value(first, second, sheet));
+    
+                if already_matched {
+                    println!("{} and {} matched already at {}.", &partip_copy[index], &partip_copy[index+1], get_value(first, second, sheet));
+                    incorrect_matches.push(partip_copy[index].clone());
+                    incorrect_matches.push(partip_copy[index+1].clone());
+    
+                }
+                else {
+                    println!("[simple_match] #{} new match: {} and {} ", index, partip_copy[index], partip_copy[index+1]);
+                    return_vec.push(partip_copy[index].clone());
+                    return_vec.push(partip_copy[index+1].clone());
+                }
+            }
+            else { // TODO make it work with odd number of people
+                let index = get_person_index(&partip_copy[index], sheet);
+                let index_usize : usize = index.try_into().unwrap();
+                println!("!!!!!!!!!!!!!!!!!!!!!!!!!!![simple_match] {} has no match! ", partip_copy[index_usize]);
+            }
+    
+            index += 2;
+        }
+
+        for it in incorrect_matches.iter() {
+            println!("Failed to match: {}, count {}", it, count);
+        }
+
+        if incorrect_matches.is_empty()
+        {
+            println!("-------------------------------------------------------------no incorrect matches, return. Times tried:{}", count);
+            break;
+        }
+        else {
+
+            println!("--------------------------------------------------------------------Retry. Times tried:{}", count);
+        }      
+    }
+
+    return return_vec;
+}
 
 fn matching(participants : &Vec<String>, sheet : &Sheet, depth: usize) -> Vec<String>{
 
-    let mut index = 0;
     let length: usize = participants.len();
+    let mut partip_copy = participants.clone();
+    let mut index = 0;
     let mut incorrect_matches: Vec<String> = Vec::new();
     let mut return_vec: Vec<String> = Vec::new();
 
     println!("matching: |\tlength {} \tdepth {}", length, depth);
+    partip_copy.shuffle(&mut thread_rng());
 
-    // FIRST PART
     while index < length {
         if length > index+1 {
 
-            let first_pos = get_person_index(&participants[index], sheet, TABLE_SIZE);
-            let second_pos = get_person_index(&participants[index+1], sheet, TABLE_SIZE);
+            let first = get_person_index(&partip_copy[index], sheet);
+            let second = get_person_index(&partip_copy[index+1], sheet);
 
-            let already_matched = sheet.value(first_pos, second_pos).as_str_or("").ne("");
+            let already_matched = get_value(first, second, sheet).ne("");
 
-            println!("{} and {} MATCH? {}.", &participants[index], &participants[index+1], sheet.value(first_pos, second_pos).as_str_or(""));
+            println!("{}:{} and {}:{} MATCH? {}.", &partip_copy[index], first, &partip_copy[index+1], second, get_value(first, second, sheet));
 
             if already_matched {
-                println!("{} and {} matched already at {}.", &participants[index], &participants[index+1], sheet.value(first_pos, second_pos).as_str_or(""));
-                incorrect_matches.push(participants[index].clone());
-                incorrect_matches.push(participants[index+1].clone());
+                //println!("{} and {} matched already at {}.", &participants[index], &participants[index+1], get_value(first, second, sheet));
+                incorrect_matches.push(partip_copy[index].clone());
+                incorrect_matches.push(partip_copy[index+1].clone());
 
             }
             else {
-                println!("{}[validate_matches] new match: {} and {} ", index, participants[index], participants[index+1]);
-                return_vec.push(participants[index].clone());
-                return_vec.push(participants[index+1].clone());
+                println!("[validate_matches] new match: {} x {}", partip_copy[index], partip_copy[index+1]);
+                return_vec.push(partip_copy[index].clone());
+                return_vec.push(partip_copy[index+1].clone());
             }
         }
         else { // TODO check if algorithm works with odd number
-            let first_pos = get_person_index(&participants[index], sheet, length.try_into().unwrap());
-            let u32u : usize;
-            u32u = first_pos.try_into().unwrap();
-            println!("[validate_matches] {} has no match! ", participants[u32u]);
+            let index = get_person_index(&partip_copy[index], sheet);
+            let index_usize : usize = index.try_into().unwrap();
+            println!("[validate_matches] {} has no match! ", partip_copy[index_usize]);
         }
 
         index += 2;
     }
 
-    for it in incorrect_matches.iter(){
-        println!("Failed matches: {}\t| depth {}", it, depth);
-    }
-    println!();
-
     if incorrect_matches.len() > 0 {
 
-        if incorrect_matches.len() == 2 // TODO randomly get a pair and try to match them
+        for it in incorrect_matches.iter(){
+            println!("Failed matches: {}\t| depth {}", it, depth);
+        }
+
+        if incorrect_matches.len() <= 4 // randomly add a pair to the pot and rematch
         {
-            // TODO FIX.
-            
-            println!("[Failed matches] Two people remaining, get LAST two people");
-            // let first = return_vec.pop().unwrap();
-            // let second = return_vec.pop().unwrap();
-            let return_vec_copy = return_vec.clone();
-            let mut deque = VecDeque::from(return_vec_copy);
-            let deque_first = deque.pop_back().unwrap();
-            let deque_second = deque.pop_back().unwrap();
-            println!("----lucky people {} {}", deque_first, deque_second);
+            let mut vec_deque = VecDeque::from(return_vec.clone());
+            let deque_first = vec_deque.pop_back().unwrap();
+            let deque_second = vec_deque.pop_back().unwrap();
+            println!("[Failed matches] less than 5 matches failed, move a match to that pot: {} x {}", deque_first, deque_second);
     
             incorrect_matches.push(deque_first);
             incorrect_matches.push(deque_second);
 
             return_vec.remove(return_vec.len()-1);
-            return_vec.remove(return_vec.len()-2);
+            return_vec.remove(return_vec.len()-1);
         }
 
-        incorrect_matches.shuffle(&mut thread_rng());
-
-        if depth < 6 {
+        if depth < MAX_DEPTH {
+            incorrect_matches.shuffle(&mut thread_rng());
             let mut new_matches = matching(&incorrect_matches, sheet, depth+1);
-            println!("New matches:");
             return_vec.append(&mut new_matches);
 
         } else {
-            println!("Depth exceeded 6, return.");
+            println!("Depth exceeded {}, return.", MAX_DEPTH);
             for it in incorrect_matches.iter(){
                 println!("Failed matches: {}\t| depth {}", it, depth);
             }
@@ -206,32 +276,18 @@ fn matching(participants : &Vec<String>, sheet : &Sheet, depth: usize) -> Vec<St
 ///
 /// # Arguments
 ///
-/// * `index` - Represents the index of the employee in `vect`
-/// * `vect` - Vector of employees participating in lottery
-/// * `table` - 2D Vector representing the Excel table
-fn get_person_index(person_name : &String, sheet : &Sheet, number_participants : u32)  -> u32 {
+/// * `person_name` - The name of the employee in participants list
+/// * `sheet` - Sheet that has the employee match
+fn get_person_index(person_name : &String, sheet : &Sheet)  -> u32 {
 
-    println!("[get_person_index_from_employee_list] person_name:{} number:{}", person_name, number_participants);
     let mut idx = 0;
 
-    for r in 0..=number_participants{
-        match sheet.value(r, 0) {
-            spreadsheet_ods::Value::Empty => {}
-            spreadsheet_ods::Value::Boolean(v) => {}
-            spreadsheet_ods::Value::Number(v) => {}
-            spreadsheet_ods::Value::Percentage(v) => {}
-            spreadsheet_ods::Value::Currency(v, cur) => {}
-            spreadsheet_ods::Value::Text(v) => {
-                //println!("mainSheet index:{} value:{}", idx, v);
-                if person_name.eq(v)
-                {
-                    println!("---{} found on table index {}", person_name, idx);
-                    return idx;
-                }
-            }
-            spreadsheet_ods::Value::TextXml(v) => {}
-            spreadsheet_ods::Value::DateTime(v) => {}
-            spreadsheet_ods::Value::TimeDuration(v) => {}
+    for r in 0..=TABLE_SIZE {
+        let name: &str = get_value(r, 0, sheet);
+        if person_name.eq(name)
+        {
+            //println!("[get_person_index] person_name:{} found index:{}", person_name, idx);
+            return idx;
         }
 
         idx += 1;
@@ -247,6 +303,7 @@ fn get_person_index(person_name : &String, sheet : &Sheet, number_participants :
 ///
 /// * `vect` - Final match list
 fn print_matches(vect : &Vec<String>, extra_print : bool) {
+
     let mut i = 0;
     let size = vect.len();
     let mut match_count = 0;
